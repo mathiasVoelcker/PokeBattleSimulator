53cbac4dc327f9dea56149a1a87c739a375735a3
Mathias Voelcker <mathias_V94@hotmail.com>
2017-09-03 15:56:12 -0300

    remove dropping migration

--- db/migrate/20170715214822_drop_database.rb
+++
@@ -1,9 +0,0 @@
-class DropDatabase < ActiveRecord::Migration[5.1]
-  def change
-    drop_table :pokemons
-    drop_table :pokemon_species
-    drop_table :attacks
-    drop_table :attack_types
-    drop_table :elements
-  end
-end
