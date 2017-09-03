5fea638c18798911d29415ea0cece7f831e93886
Mathias Voelcker <mathias_V94@hotmail.com>
2017-09-03 13:49:25 -0300

    erase test from app

--- test/controllers/attacks_controller_test.rb
+++ test/controllers/attacks_controller_test.rb
@@ -1,48 +1,5 @@
 require 'test_helper'
 
 class AttacksControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @attack = attacks(:one)
-  end
 
-  test "should get index" do
-    get attacks_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get new_attack_url
-    assert_response :success
-  end
-
-  test "should create attack" do
-    assert_difference('Attack.count') do
-      post attacks_url, params: { attack: { base_power: @attack.base_power, element_id: @attack.element_id, name: @attack.name, attack_type_id: @attack.attack_type_id } }
-    end
-
-    assert_redirected_to attack_url(Attack.last)
-  end
-
-  test "should show attack" do
-    get attack_url(@attack)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_attack_url(@attack)
-    assert_response :success
-  end
-
-  test "should update attack" do
-    patch attack_url(@attack), params: { attack: { base_power: @attack.base_power, element_id: @attack.element_id, name: @attack.name, attack_type_id: @attack.attack_type_id } }
-    assert_redirected_to attack_url(@attack)
-  end
-
-  test "should destroy attack" do
-    assert_difference('Attack.count', -1) do
-      delete attack_url(@attack)
-    end
-
-    assert_redirected_to attacks_url
-  end
 end
