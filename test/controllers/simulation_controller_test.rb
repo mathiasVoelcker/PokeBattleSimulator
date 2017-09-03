45a09508b5d8646c3fcf61a81a744c8128801b77
Mathias Voelcker <mathias_V94@hotmail.com>
2017-09-03 13:43:20 -0300

    ajust pokemon_species test yml

--- test/controllers/simulation_controller_test.rb
+++ test/controllers/simulation_controller_test.rb
@@ -1,19 +1,5 @@
 require 'test_helper'
 
 class SimulationControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get simulation_index_url
-    assert_response :success
-  end
-
-  test "should get create" do
-    get simulation_create_url
-    assert_response :success
-  end
-
-  test "should get show" do
-    get simulation_show_url
-    assert_response :success
-  end
 
 end
